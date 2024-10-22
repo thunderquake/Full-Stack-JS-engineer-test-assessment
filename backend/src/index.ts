43d6167cd6cd8f8a92df67dd06ea01b97917c191
Anna Dmytriieva <loooverrr12@gmail.com>
2024-10-22 18:47:55 +0300

    feat: add main routes

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express, { Express } from "express";
+import { appRouter } from "./router";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(appRouter);
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
