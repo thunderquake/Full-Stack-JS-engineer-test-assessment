792c8bba8de47f13a84096b3b256e1287f784d5f
Anna Dmytriieva <loooverrr12@gmail.com>
2024-10-22 20:04:59 +0300

    feat: set up front + back connection

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import CountriesList from "@/components/CountriesList";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Country Info App</h1>
+      <CountriesList />
+    </div>
+  );
+}
